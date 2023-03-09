0f1700c2cd1bac2d5a0a57ee6419640bcd9398a1
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-09 17:52:15 +0900

    feat(transform): add response transformer for range headers

--- transform.ts
+++ transform.ts
@@ -1,16 +1,28 @@
+// Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
+// This module is browser compatible.
+
 import {
   ConditionalHeader,
   isErr,
+  isIntRange,
   isNull,
+  isOtherRange,
   Method,
-  NoContentHeaders,
+  parse,
   RangeHeader,
+  type RangeSpec,
+  RangesSpecifier,
   RepresentationHeader,
   Status,
   unsafe,
 } from "./deps.ts";
-import { parseRange } from "./parser.ts";
-import type { Range, RangeSpec, RangeUnit } from "./types.ts";
+import {
+  RangeUnit as Unit,
+  RequestedRangeNotSatisfiableResponse,
+  shallowMergeHeaders,
+} from "./util.ts";
+
+import type { Range, RangeUnit, Specifier } from "./types.ts";
 
 export function withAcceptRanges(
   response: Response,
@@ -42,14 +54,12 @@ export async function withContentRange(
     isNull(rangeValue) ||
     request.headers.has(ConditionalHeader.IfRange) ||
     response.headers.has(RangeHeader.ContentRange) ||
-    response.headers.get(RangeHeader.AcceptRanges) === "none" ||
+    response.headers.get(RangeHeader.AcceptRanges) === Unit.None ||
     response.bodyUsed ||
     isNull(contentType)
-  ) {
-    return response;
-  }
+  ) return response;
 
-  const rangeContainer = unsafe(() => parseRange(rangeValue));
+  const rangeContainer = unsafe(() => parse(rangeValue));
 
   if (isErr(rangeContainer)) {
     // A server that supports range requests MAY ignore or reject a Range header field that contains an invalid ranges-specifier (Section 14.1.1), a ranges-specifier with more than two overlapping ranges, or a set of many small ranges that are not listed in ascending order, since these are indications of either a broken client or a deliberate denial-of-service attack (Section 17.15).
@@ -58,51 +68,71 @@ export async function withContentRange(
   }
 
   const parsedRange = rangeContainer.value;
-  const ranges = Array.from(context.ranges);
-  const maybeRange = ranges.find(matchRange);
-
+  const validityResult = checkValidity(parsedRange, context.ranges);
   // An origin server MUST ignore a Range header field that contains a range unit it does not understand. A proxy MAY discard a Range header field that contains a range unit it does not understand.
   // @see https://www.rfc-editor.org/rfc/rfc9110#section-14.2-5
-  if (!maybeRange) return response;
+  if (!validityResult) return response;
 
-  const matchedRange = maybeRange;
+  const matchedRange = validityResult;
   const body = await response.clone().arrayBuffer();
   const satisfiableRangeSet = parsedRange.rangeSet.filter(isSatisfiable);
 
   if (!satisfiableRangeSet.length) {
-    const contentRange = `${matchedRange.unit} */${body.byteLength}`;
-    const headers = new NoContentHeaders(response.headers);
-
-    headers.set(RangeHeader.ContentRange, contentRange);
-
-    return new Response(null, {
-      status: Status.RequestedRangeNotSatisfiable,
-      headers,
-    });
+    return new RequestedRangeNotSatisfiableResponse({
+      rangeUnit: validityResult.unit,
+      completeLength: body.byteLength,
+    }, { headers: response.headers });
   }
 
-  const evaluateResult = await matchedRange.evaluate({
-    rangeSpecs: parsedRange.rangeSet,
-    contents: body,
+  const partialContents = await validityResult.partial({
+    rangeSet: parsedRange.rangeSet,
+    content: body,
     contentType,
   });
-  const headers = new Headers(response.headers);
+  const headers = shallowMergeHeaders(
+    response.headers,
+    partialContents.headers,
+  );
 
-  headers.set(RangeHeader.ContentRange, evaluateResult.contentRange);
-
-  return new Response(evaluateResult.partialContents, {
+  return new Response(partialContents.content, {
     status: Status.PartialContent,
     headers,
   });
 
+  function isSatisfiable(rangeSpec: RangeSpec): boolean {
+    return matchedRange.isSatisfiable({ contents: body, rangeSpec });
+  }
+}
+
+function toSpecifier(
+  rangeSpec: RangeSpec,
+): Specifier {
+  if (isOtherRange(rangeSpec)) return "other-range";
+  if (isIntRange(rangeSpec)) return "int-range";
+
+  return "suffix-range";
+}
+
+function checkValidity(
+  range: RangesSpecifier,
+  ranges: Iterable<Range>,
+): false | Range {
+  const parsedRange = range;
+  const maybeRange = Array.from(ranges).find(matchRange);
+
+  // An origin server MUST ignore a Range header field that contains a range unit it does not understand. A proxy MAY discard a Range header field that contains a range unit it does not understand.
+  // @see https://www.rfc-editor.org/rfc/rfc9110#section-14.2-5
+  if (!maybeRange) return false;
+
+  const matchedRange = maybeRange;
+  const nodes = parsedRange.rangeSet.map(toSpecifier);
+  const isValid = nodes.every((v) => matchedRange.specifiers.includes(v));
+
+  if (!isValid) return false;
+
+  return maybeRange;
+
   function matchRange(range: Range): boolean {
     return range.unit === parsedRange.rangeUnit;
-  }
-
-  function isSatisfiable(rangeSpec: RangeSpec): boolean {
-    return matchedRange.isSatisfiable({
-      contents: body,
-      rangeSpec,
-    });
   }
 }
---
+++ transform_test.ts
@@ -0,0 +1,260 @@
+import { withAcceptRanges, withContentRange } from "./transform.ts";
+import {
+  assert,
+  ConditionalHeader,
+  describe,
+  equalsResponse,
+  it,
+  RangeHeader,
+  Status,
+} from "./_dev_deps.ts";
+import type { RangeUnit } from "./types.ts";
+
+describe("withContentRange", () => {
+  describe("should return same response", () => {
+    it("if the request is not GET method", async () => {
+      const initResponse = new Response();
+      const response = await withContentRange(
+        new Request("test:", { method: "HEAD" }),
+        initResponse,
+        { ranges: [] },
+      );
+
+      assert(initResponse === response);
+    });
+
+    it("if the Range header does not exist in request", async () => {
+      const initResponse = new Response();
+      const response = await withContentRange(
+        new Request("test:"),
+        initResponse,
+        { ranges: [] },
+      );
+
+      assert(initResponse === response);
+    });
+
+    it("if the If-Range header exists in request", async () => {
+      const initResponse = new Response();
+      const response = await withContentRange(
+        new Request("test:", {
+          headers: {
+            [RangeHeader.Range]: "bytes=0-",
+            [ConditionalHeader.IfRange]: "",
+          },
+        }),
+        initResponse,
+        { ranges: [] },
+      );
+
+      assert(initResponse === response);
+    });
+
+    it("if the Content-Range header exists in response", async () => {
+      const initResponse = new Response(null, {
+        headers: { [RangeHeader.ContentRange]: "" },
+      });
+      const response = await withContentRange(
+        new Request("test:", {
+          headers: { [RangeHeader.Range]: "bytes=0-" },
+        }),
+        initResponse,
+        { ranges: [] },
+      );
+
+      assert(initResponse === response);
+    });
+
+    it("if the Accept-Ranges header is none in response", async () => {
+      const initResponse = new Response(null, {
+        headers: { [RangeHeader.AcceptRanges]: "none" },
+      });
+      const response = await withContentRange(
+        new Request("test:", { headers: { [RangeHeader.Range]: "bytes=0-" } }),
+        initResponse,
+        { ranges: [] },
+      );
+
+      assert(initResponse === response);
+    });
+
+    it("if the response body has been read", async () => {
+      const initResponse = new Response("");
+      await initResponse.text();
+
+      assert(initResponse.bodyUsed);
+
+      const response = await withContentRange(
+        new Request("test:", { headers: { [RangeHeader.Range]: "bytes=0-" } }),
+        initResponse,
+        { ranges: [] },
+      );
+
+      assert(initResponse === response);
+    });
+
+    it("if the parsing Range header is fail", async () => {
+      const initResponse = new Response("");
+
+      const response = await withContentRange(
+        new Request("test:", { headers: { [RangeHeader.Range]: "<invalid>" } }),
+        initResponse,
+        { ranges: [] },
+      );
+
+      assert(initResponse === response);
+    });
+
+    it("if the ranges are not match", async () => {
+      const initResponse = new Response("");
+
+      const response = await withContentRange(
+        new Request("test:", { headers: { [RangeHeader.Range]: "bytes=0-" } }),
+        initResponse,
+        { ranges: [] },
+      );
+
+      assert(initResponse === response);
+    });
+
+    it("if the range unit does not match", async () => {
+      const initResponse = new Response("");
+
+      const response = await withContentRange(
+        new Request("test:", { headers: { [RangeHeader.Range]: "bytes=0-" } }),
+        initResponse,
+        {
+          ranges: [{
+            unit: "xxx",
+            specifiers: ["int-range"],
+            isSatisfiable: () => true,
+            partial: () => ({ content: "", headers: new Headers() }),
+          }],
+        },
+      );
+
+      assert(initResponse === response);
+    });
+
+    it("if the range specifier does not match", async () => {
+      const initResponse = new Response("");
+
+      const response = await withContentRange(
+        new Request("test:", { headers: { [RangeHeader.Range]: "bytes=0-" } }),
+        initResponse,
+        {
+          ranges: [{
+            unit: "bytes",
+            specifiers: ["other-range"],
+            isSatisfiable: () => true,
+            partial: () => ({ content: "", headers: new Headers() }),
+          }],
+        },
+      );
+
+      assert(initResponse === response);
+    });
+  });
+
+  it("should return 413 response if the specifiers include invalid specifier", async () => {
+    const initResponse = new Response("abcd");
+
+    const response = await withContentRange(
+      new Request("test:", { headers: { [RangeHeader.Range]: "bytes=0-" } }),
+      initResponse,
+      {
+        ranges: [{
+          unit: "bytes",
+          specifiers: ["int-range"],
+          isSatisfiable: () => false,
+          partial: () => ({ content: "", headers: new Headers() }),
+        }],
+      },
+    );
+
+    assert(
+      await equalsResponse(
+        response,
+        new Response(null, {
+          status: Status.RequestedRangeNotSatisfiable,
+          headers: {
+            [RangeHeader.ContentRange]: "bytes */4",
+          },
+        }),
+        true,
+      ),
+    );
+  });
+
+  it("should return 206 response", async () => {
+    const initResponse = new Response("abcd");
+
+    const response = await withContentRange(
+      new Request("test:", { headers: { [RangeHeader.Range]: "bytes=0-" } }),
+      initResponse,
+      {
+        ranges: [{
+          unit: "bytes",
+          specifiers: ["int-range"],
+          isSatisfiable: () => true,
+          partial: () => ({
+            content: "efgh",
+            headers: new Headers({ [RangeHeader.ContentRange]: "range" }),
+          }),
+        }],
+      },
+    );
+
+    assert(
+      await equalsResponse(
+        response,
+        new Response("efgh", {
+          status: Status.PartialContent,
+          headers: {
+            ["content-type"]: "text/plain;charset=UTF-8",
+            [RangeHeader.ContentRange]: "range",
+          },
+        }),
+        true,
+      ),
+    );
+  });
+});
+
+describe("withAcceptRanges", () => {
+  it("should return same response if the response has Accept-Ranges header", () => {
+    const initResponse = new Response(null, {
+      headers: { [RangeHeader.AcceptRanges]: "" },
+    });
+    const response = withAcceptRanges(initResponse, "none");
+
+    assert(response === initResponse);
+  });
+
+  it("should return response what has Accept-Ranges header", async () => {
+    const table: [Response, RangeUnit, Response][] = [
+      [
+        new Response(),
+        "bytes",
+        new Response(null, {
+          headers: { [RangeHeader.AcceptRanges]: "bytes" },
+        }),
+      ],
+      [
+        new Response(),
+        "none",
+        new Response(null, {
+          headers: { [RangeHeader.AcceptRanges]: "none" },
+        }),
+      ],
+    ];
+
+    await Promise.all(table.map(async ([initResponse, unit, expected]) => {
+      const response = withAcceptRanges(initResponse, unit);
+
+      assert(
+        await equalsResponse(response, expected, true),
+      );
+    }));
+  });
+});
