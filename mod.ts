8687894131197c1c9a2bb95df29fd7cd7c4487cf
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-22 22:33:47 +0900

    feat(mod): export types of interface

--- mod.ts
+++ mod.ts
@@ -1,5 +1,14 @@
 // Copyright 2023-latest the httpland authors. All rights reserved. MIT license.
 // This module is browser compatible.
 
-export { acceptRanges, contentRange, rangeRequest } from "./middleware.ts";
-export { type Handler } from "./deps.ts";
+export { rangeRequest } from "./middleware.ts";
+export {
+  type Handler,
+  type IntRange,
+  type Middleware,
+  type OtherRange,
+  type RangeSet,
+  type RangeSpec,
+  type SuffixRange,
+} from "./deps.ts";
+export { type Range, type RangeContext, type Respond } from "./types.ts";
