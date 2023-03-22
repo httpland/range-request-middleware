909f35519eb68390e4839f87f2b828a1a39753ce
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-22 14:07:52 +0900

    chore(ranges): add multipart byteranges utility

--- ranges/utils.ts
+++ ranges/utils.ts
@@ -2,38 +2,43 @@
 // This module is browser compatible.
 
 import { concat, isString } from "../deps.ts";
+import { InclRange, stringify } from "../content_range.ts";
 
-export interface InclRange {
-  readonly firstPos: number;
-  readonly lastPos: number;
-}
-
-interface Args {
+export interface MultipartByteranges {
   readonly content: ArrayBuffer;
   readonly contentType: string;
-  readonly ranges: readonly InclRange[];
+  readonly ranges: readonly [InclRange, ...readonly InclRange[]];
+  readonly rangeUnit: string;
   readonly boundary: string;
 }
 
-export function multipartByteranges(args: Args): Uint8Array {
-  const { content, contentType, ranges, boundary } = args;
+/** Deserialize {@link MultipartByteranges} into multipart/byterages bytestream. */
+export function multipartByteranges(args: MultipartByteranges): ArrayBuffer {
+  const { content, contentType, ranges, boundary, rangeUnit } = args;
   const size = content.byteLength;
   const boundaryDelimiter = "--" + boundary;
   const endDelimiter = boundaryDelimiter + "--";
   const encoder = new TextEncoder();
+  const contentTypeStr = `Content-Type: ${contentType}`;
   const contents = ranges
     .map(bodyParts)
     .flat()
     .map((buffer) => new Uint8Array(buffer))
     .concat(encoder.encode(endDelimiter));
+  const buffer = join(contents, encoder.encode("\n")).buffer;
 
-  return join(contents, encoder.encode("\n"));
+  return buffer;
 
   function bodyParts(range: InclRange): ArrayBuffer[] {
+    const contentRangeStr = stringify({
+      rangeUnit,
+      range: { ...range, completeLength: size },
+    });
+
     return [
       boundaryDelimiter,
-      `Content-Type: ${contentType}`,
-      `Content-Range: ${range.firstPos}-${range.lastPos}/${size}`,
+      contentTypeStr,
+      `Content-Range: ${contentRangeStr}`,
       "",
       content.slice(range.firstPos, range.lastPos + 1),
     ].map(toBuffer);
