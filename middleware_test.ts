ad77c0c6f054fd96f216d0b6ed52ad3c26aaae5f
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-22 17:08:19 +0900

    test(middleware): add test case

--- middleware_test.ts
+++ middleware_test.ts
@@ -1,11 +1,14 @@
-import { rangeRequest } from "./middleware.ts";
+import { acceptRanges, contentRange, rangeRequest } from "./middleware.ts";
 import {
   assert,
+  assertSpyCalls,
+  ConditionalHeader,
   describe,
   equalsResponse,
   it,
   RangeHeader,
   RepresentationHeader,
+  spy,
   Status,
 } from "./_dev_deps.ts";
 
@@ -101,6 +104,253 @@ vwxyz
             },
           },
         ),
+        true,
+      ),
+    );
+  });
+});
+
+describe("contentRange", () => {
+  it("should return same response if the request is not GET", async () => {
+    const middleware = contentRange();
+    const initResponse = new Response();
+    const handler = spy(() => initResponse);
+
+    const response = await middleware(
+      new Request("test:", { method: "ANY" }),
+      handler,
+    );
+
+    assertSpyCalls(handler, 1);
+    assert(response === initResponse);
+  });
+
+  it("should return same response if the request is not range request", async () => {
+    const middleware = contentRange();
+    const initResponse = new Response();
+    const handler = spy(() => initResponse);
+
+    const response = await middleware(
+      new Request("test:"),
+      handler,
+    );
+
+    assertSpyCalls(handler, 1);
+    assert(response === initResponse);
+  });
+
+  it("should return same response if the request has If-Range header", async () => {
+    const middleware = contentRange();
+    const initResponse = new Response();
+    const handler = spy(() => initResponse);
+
+    const response = await middleware(
+      new Request("test:", {
+        headers: {
+          [ConditionalHeader.IfRange]: "",
+          [RangeHeader.Range]: "bytes=0-1",
+        },
+      }),
+      handler,
+    );
+
+    assertSpyCalls(handler, 1);
+    assert(response === initResponse);
+  });
+
+  it("should return same response if the request range header is invalid", async () => {
+    const middleware = contentRange();
+    const initResponse = new Response();
+    const handler = spy(() => initResponse);
+
+    const response = await middleware(
+      new Request("test:", {
+        headers: {
+          [RangeHeader.Range]: "",
+        },
+      }),
+      handler,
+    );
+
+    assertSpyCalls(handler, 1);
+    assert(response === initResponse);
+  });
+
+  it("should return 416 response if the range request is not supported specifier", async () => {
+    const middleware = contentRange([]);
+    const initResponse = new Response("abcdefg", {
+      headers: {
+        [RepresentationHeader.ContentType]: "text/test",
+        "x-test": "test",
+      },
+    });
+    const handler = spy(() => initResponse);
+
+    const response = await middleware(
+      new Request("test:", {
+        headers: {
+          [RangeHeader.Range]: "bytes=-100",
+        },
+      }),
+      handler,
+    );
+
+    assertSpyCalls(handler, 1);
+    assert(
+      await equalsResponse(
+        response,
+        new Response(null, {
+          status: Status.RequestedRangeNotSatisfiable,
+          headers: {
+            [RangeHeader.ContentRange]: "bytes */7",
+            "x-test": "test",
+          },
+        }),
+        true,
+      ),
+    );
+  });
+
+  it("should return 206 response if the request is single range request", async () => {
+    const middleware = contentRange();
+    const initResponse = new Response("abcdefg", {
+      headers: {
+        [RepresentationHeader.ContentType]: "text/test",
+        "x-test": "test",
+      },
+    });
+    const handler = spy(() => initResponse);
+
+    const response = await middleware(
+      new Request("test:", {
+        headers: {
+          [RangeHeader.Range]: "bytes=-100",
+        },
+      }),
+      handler,
+    );
+
+    assertSpyCalls(handler, 1);
+    assert(
+      await equalsResponse(
+        response,
+        new Response("abcdefg", {
+          status: Status.PartialContent,
+          headers: {
+            [RepresentationHeader.ContentType]: "text/test",
+            [RangeHeader.ContentRange]: "bytes 0-6/7",
+            "x-test": "test",
+          },
+        }),
+        true,
+      ),
+    );
+  });
+
+  it("should return 206 response if the request is multiple range request", async () => {
+    const middleware = contentRange();
+    const initResponse = new Response("abcdefg", {
+      headers: {
+        [RepresentationHeader.ContentType]: "text/test",
+        "x-test": "test",
+      },
+    });
+    const handler = spy(() => initResponse);
+
+    const response = await middleware(
+      new Request("test:", {
+        headers: {
+          [RangeHeader.Range]: "bytes=-100, 0-1",
+        },
+      }),
+      handler,
+    );
+
+    assertSpyCalls(handler, 1);
+    assert(
+      await equalsResponse(
+        response,
+        new Response(
+          `--2fb5e13419fc89246865e7a324f476ec624e8740
+Content-Type: text/test
+Content-Range: bytes 0-6/7
+
+abcdefg
+--2fb5e13419fc89246865e7a324f476ec624e8740
+Content-Type: text/test
+Content-Range: bytes 0-1/7
+
+ab
+--2fb5e13419fc89246865e7a324f476ec624e8740--`,
+          {
+            status: Status.PartialContent,
+            headers: {
+              [RepresentationHeader.ContentType]:
+                "multipart/byteranges; boundary=2fb5e13419fc89246865e7a324f476ec624e8740",
+              "x-test": "test",
+            },
+          },
+        ),
+        true,
+      ),
+    );
+  });
+});
+
+describe("acceptRanges", () => {
+  it("should return response what includes accept-ranges header", async () => {
+    const middleware = acceptRanges();
+
+    const response = await middleware(
+      new Request("test:"),
+      () => new Response(),
+    );
+
+    assert(
+      await equalsResponse(
+        response,
+        new Response(null, {
+          headers: { [RangeHeader.AcceptRanges]: "bytes" },
+        }),
+        true,
+      ),
+    );
+  });
+
+  it("should return response what includes accept-ranges header thay is none", async () => {
+    const middleware = acceptRanges(["none"]);
+
+    const response = await middleware(
+      new Request("test:"),
+      () => new Response(),
+    );
+
+    assert(
+      await equalsResponse(
+        response,
+        new Response(null, {
+          headers: { [RangeHeader.AcceptRanges]: "none" },
+        }),
+        true,
+      ),
+    );
+  });
+
+  it("should return same response if the response has Accept-Ranges header", async () => {
+    const middleware = acceptRanges();
+
+    const response = await middleware(
+      new Request("test:"),
+      () =>
+        new Response(null, { headers: { [RangeHeader.AcceptRanges]: "xxx" } }),
+    );
+
+    assert(
+      await equalsResponse(
+        response,
+        new Response(null, {
+          headers: { [RangeHeader.AcceptRanges]: "xxx" },
+        }),
         true,
       ),
     );
